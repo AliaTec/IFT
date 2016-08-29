7aa030811dd196b9dd2b66688d1097768d195902
EduardoAliatec <edcruz@aliatec.com.mx>
2016-08-29 16:53:10 -0500

    INFOTEC
    NUEVOS LAYOUTS DE PAGO

--- Aliatec-ITX-IFT/Dispersion/Items.vb
+++ Aliatec-ITX-IFT/Dispersion/Items.vb
@@ -10,13 +10,13 @@ Public Class Items
 
         Dim newRow As DataRow
 
-        newRow = Me.NewRow()
-        newRow.Item("Label") = 100014
-        'accion
-        newRow.Item("Accion") = 100014
-        'case del aspx
-        newRow.Item("Case") = 100014
-        Me.Rows.Add(newRow)
+        'newRow = Me.NewRow()
+        'newRow.Item("Label") = 100014
+        ''accion
+        'newRow.Item("Accion") = 100014
+        ''case del aspx
+        'newRow.Item("Case") = 100014
+        'Me.Rows.Add(newRow)
 
         newRow = Me.NewRow()
         newRow.Item("Label") = 100016
@@ -34,13 +34,13 @@ Public Class Items
         newRow.Item("Case") = 100017
         Me.Rows.Add(newRow)
 
-        newRow = Me.NewRow()
-        newRow.Item("Label") = 100019
-        'accion
-        newRow.Item("Accion") = 100019
-        'case del aspx
-        newRow.Item("Case") = 100019
-        Me.Rows.Add(newRow)
+        'newRow = Me.NewRow()
+        'newRow.Item("Label") = 100019
+        ''accion
+        'newRow.Item("Accion") = 100019
+        ''case del aspx
+        'newRow.Item("Case") = 100019
+        'Me.Rows.Add(newRow)
 
         newRow = Me.NewRow()
         newRow.Item("Label") = 100020
@@ -50,53 +50,53 @@ Public Class Items
         newRow.Item("Case") = 100020
         Me.Rows.Add(newRow)
 
-        newRow = Me.NewRow()
-        newRow.Item("Label") = 100021
-        'accion
-        newRow.Item("Accion") = 100021
-        'case del aspx
-        newRow.Item("Case") = 100021
-        Me.Rows.Add(newRow)
+        'newRow = Me.NewRow()
+        'newRow.Item("Label") = 100021
+        ''accion
+        'newRow.Item("Accion") = 100021
+        ''case del aspx
+        'newRow.Item("Case") = 100021
+        'Me.Rows.Add(newRow)
 
-        newRow = Me.NewRow()
-        newRow.Item("Label") = 100022
-        'accion
-        newRow.Item("Accion") = 100022
-        'case del aspx
-        newRow.Item("Case") = 100022
-        Me.Rows.Add(newRow)
+        'newRow = Me.NewRow()
+        'newRow.Item("Label") = 100022
+        ''accion
+        'newRow.Item("Accion") = 100022
+        ''case del aspx
+        'newRow.Item("Case") = 100022
+        'Me.Rows.Add(newRow)
 
-        newRow = Me.NewRow()
-        newRow.Item("Label") = 100023
-        'accion
-        newRow.Item("Accion") = 100023
-        'case del aspx
-        newRow.Item("Case") = 100023
-        Me.Rows.Add(newRow)
+        'newRow = Me.NewRow()
+        'newRow.Item("Label") = 100023
+        ''accion
+        'newRow.Item("Accion") = 100023
+        ''case del aspx
+        'newRow.Item("Case") = 100023
+        'Me.Rows.Add(newRow)
 
-        newRow = Me.NewRow()
-        newRow.Item("Label") = 100024
-        'accion
-        newRow.Item("Accion") = 100024
-        'case del aspx
-        newRow.Item("Case") = 100024
-        Me.Rows.Add(newRow)
+        'newRow = Me.NewRow()
+        'newRow.Item("Label") = 100024
+        ''accion
+        'newRow.Item("Accion") = 100024
+        ''case del aspx
+        'newRow.Item("Case") = 100024
+        'Me.Rows.Add(newRow)
 
-        newRow = Me.NewRow()
-        newRow.Item("Label") = 100025
-        'accion
-        newRow.Item("Accion") = 100025
-        'case del aspx
-        newRow.Item("Case") = 100025
-        Me.Rows.Add(newRow)
+        'newRow = Me.NewRow()
+        'newRow.Item("Label") = 100025
+        ''accion
+        'newRow.Item("Accion") = 100025
+        ''case del aspx
+        'newRow.Item("Case") = 100025
+        'Me.Rows.Add(newRow)
 
-        newRow = Me.NewRow()
-        newRow.Item("Label") = 100026
-        'accion
-        newRow.Item("Accion") = 100026
-        'case del aspx
-        newRow.Item("Case") = 100026
-        Me.Rows.Add(newRow)
+        'newRow = Me.NewRow()
+        'newRow.Item("Label") = 100026
+        ''accion
+        'newRow.Item("Accion") = 100026
+        ''case del aspx
+        'newRow.Item("Case") = 100026
+        'Me.Rows.Add(newRow)
 
         newRow = Me.NewRow()
         newRow.Item("Label") = 100027
@@ -122,20 +122,113 @@ Public Class Items
         newRow.Item("Case") = 100029
         Me.Rows.Add(newRow)
 
+        'newRow = Me.NewRow()
+        'newRow.Item("Label") = 100031
+        ''accion
+        'newRow.Item("Accion") = 100031
+        ''case del aspx
+        'newRow.Item("Case") = 100031
+        'Me.Rows.Add(newRow)
+
+        ''''''''NUEVOS LAYOUTS''''''
+
+        'Layout_Altas_Interbancario
         newRow = Me.NewRow()
-        newRow.Item("Label") = 100030
+        newRow.Item("Label") = 100118
         'accion
-        newRow.Item("Accion") = 100030
+        newRow.Item("Accion") = 100118
         'case del aspx
-        newRow.Item("Case") = 100030
+        newRow.Item("Case") = 100118
         Me.Rows.Add(newRow)
 
+        'Layout_Altas_Bancomer
         newRow = Me.NewRow()
-        newRow.Item("Label") = 100031
+        newRow.Item("Label") = 100119
         'accion
-        newRow.Item("Accion") = 100031
+        newRow.Item("Accion") = 100119
         'case del aspx
-        newRow.Item("Case") = 100031
+        newRow.Item("Case") = 100119
+        Me.Rows.Add(newRow)
+
+        'Layout_Nómina_Interbancario
+        newRow = Me.NewRow()
+        newRow.Item("Label") = 100120
+        'accion
+        newRow.Item("Accion") = 100120
+        'case del aspx
+        newRow.Item("Case") = 100120
+        Me.Rows.Add(newRow)
+
+        'Layout_Nómina_Bancomer
+        newRow = Me.NewRow()
+        newRow.Item("Label") = 100121
+        'accion
+        newRow.Item("Accion") = 100121
+        'case del aspx
+        newRow.Item("Case") = 100121
+        Me.Rows.Add(newRow)
+
+        'Layout_Nómina_Interbancario_Liberados
+        newRow = Me.NewRow()
+        newRow.Item("Label") = 100122
+        'accion
+        newRow.Item("Accion") = 100122
+        'case del aspx
+        newRow.Item("Case") = 100122
+        Me.Rows.Add(newRow)
+
+        'Layout_Nómina_Bancomer_Liberados
+        newRow = Me.NewRow()
+        newRow.Item("Label") = 100123
+        'accion
+        newRow.Item("Accion") = 100123
+        'case del aspx
+        newRow.Item("Case") = 100123
+        Me.Rows.Add(newRow)
+
+        'Layout_SPEI_Interbancario
+        newRow = Me.NewRow()
+        newRow.Item("Label") = 100124
+        'accion
+        newRow.Item("Accion") = 100124
+        'case del aspx
+        newRow.Item("Case") = 100124
+        Me.Rows.Add(newRow)
+
+        'Layout_SPEI_Bancomer
+        newRow = Me.NewRow()
+        newRow.Item("Label") = 100125
+        'accion
+        newRow.Item("Accion") = 100125
+        'case del aspx
+        newRow.Item("Case") = 100125
+        Me.Rows.Add(newRow)
+
+        ''Layout_SPEI_Interbancario_Liberados
+        'newRow = Me.NewRow()
+        'newRow.Item("Label") = 100126
+        ''accion
+        'newRow.Item("Accion") = 100126
+        ''case del aspx
+        'newRow.Item("Case") = 100126
+        'Me.Rows.Add(newRow)
+
+        'Layout_SPEI_Bancomer_Liberados
+        newRow = Me.NewRow()
+        newRow.Item("Label") = 100127
+        'accion
+        newRow.Item("Accion") = 100127
+        'case del aspx
+        newRow.Item("Case") = 100127
+        Me.Rows.Add(newRow)
+
+        'Layout_SPEI_Interbancario_Liberados
+        newRow = Me.NewRow()
+        newRow.Item("Label") = 100128
+        'accion
+        newRow.Item("Accion") = 100128
+        'case del aspx
+        newRow.Item("Case") = 100128
         Me.Rows.Add(newRow)
 
     End Sub
